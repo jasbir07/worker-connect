983cf8d314611241767c4f3e5c036383e5431662
jasbir07 <thakurjasbir890@gmail.com>
2025-12-30 10:54:58 +0530

    Day 3: Login and JWT authentication

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,11 +3,18 @@ const cors = require("cors");
 
 const app = express();
 const authRoutes = require("./routes/authRoutes");
+const { protect } = require("./middlewares/authMiddleware");
 // middleware
 app.use(cors());
 app.use(express.json());
 app.use("/api/auth", authRoutes);
 // test route
+app.get("/api/protected", protect, (req, res) => {
+  res.json({
+    message: "Access granted",
+    user: req.user
+  });
+});
 app.get("/", (req, res) => {
   res.send("Worker-Connect Backend Running");
 });
--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -1,5 +1,6 @@
 const User = require("../models/User");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 exports.register = async (req, res) => {
   try {
@@ -22,6 +23,40 @@ exports.register = async (req, res) => {
     res.status(201).json({
       message: "User registered successfully",
       user
+    });
+  } catch (error) {
+    res.status(500).json({ message: "Server error" });
+  }
+};
+
+exports.login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({ message: "Invalid credentials" });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(400).json({ message: "Invalid credentials" });
+    }
+
+    const token = jwt.sign(
+      { id: user._id, role: user.role },
+      process.env.JWT_SECRET,
+      { expiresIn: "1d" }
+    );
+
+    res.json({
+      message: "Login successful",
+      token,
+      user: {
+        id: user._id,
+        name: user.name,
+        role: user.role
+      }
     });
   } catch (error) {
     res.status(500).json({ message: "Server error" });
